e298b146cfb5880c5ad2ab8f1457a4104da0f9df
Maxatom <710909333@qq.com>
2019-03-26 20:39:13 +0800

    realTest

--- src/leetcode/realtest/realTest20190324/BinaryStringWithSubstringsRepresenting1ToN.java
+++ src/leetcode/realtest/realTest20190324/BinaryStringWithSubstringsRepresenting1ToN.java
@@ -1,18 +1,81 @@
 package leetcode.realtest.realTest20190324;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author shibing
  * @since 2019/3/24 10:42
  */
 public class BinaryStringWithSubstringsRepresenting1ToN {
     public static void main(String[] args) {
+        BinaryStringWithSubstringsRepresenting1ToN binary=new BinaryStringWithSubstringsRepresenting1ToN();
+        String S="0110"; int N=4;
+//        S="0110"; N=3;
+//        S="1011100"; N=7;
+//        S="1111000101";N= 5;
+        System.out.println(binary.queryString(S, N));
+//        System.out.println(binary.queryString1(S, N));
+        System.out.println(binary.queryString2(S, N));
+    }
 
+    public boolean queryString2(String S, int N) {
+        for (int i = N; i >N/2 ; i--)
+            if(!S.contains(Integer.toBinaryString(i))) return false;
+        return true;
+    }
+
+    //simplifying code
+    public boolean queryString1(String S, int N) {
+        char[] chars=S.toCharArray();
+        int n=N>>1;
+        int len=Integer.toBinaryString(n+1).length();
+        int maxlen=Integer.toBinaryString(N).length();
+        for (int i = len; i <= maxlen; i++) {
+            Set<Integer> sets=new HashSet<>();
+            int total;
+            if(i==maxlen) total=N-(1<<(i-1))+1;
+            else total=(1<<(i-1));
+            for (int j = 0; j+i-1 < chars.length; j++) {
+                if(chars[j]=='0') continue;
+                int num=toInt(chars, j, j+i);
+                if(!sets.contains(num) && num<=N)
+                    sets.add(num);
+            }
+            if(sets.size()!=total) return false;
+        }
+        return true;
     }
 
 
+    //O(S^2)
     public boolean queryString(String S, int N) {
-        String str=Integer.toBinaryString(N);
-        System.out.println(str);
-        return false;
+        char[] chars=S.toCharArray();
+        int n=N>>1;
+        int len=Integer.toBinaryString(n+1).length();
+        int maxlen=Integer.toBinaryString(N).length();
+        for (int i = len; i <= maxlen; i++) {
+            Set<Integer> sets=new HashSet<>();
+            int num=toInt(chars, 0, i);
+            if(chars[0]=='1' && num<=N) sets.add(num);
+            int total;
+            if(i==maxlen) total=N-(1<<(i-1))+1;
+            else total=(1<<(i-1));
+            for (int j = 1; j+i-1 < chars.length; j++) {
+                num=((((1<<(i-1))-1)&num)<<1)+chars[j+i-1]-'0';
+                if(chars[j]=='0') continue;
+                if(!sets.contains(num) && num<=N)
+                    sets.add(num);
+            }
+            if(sets.size()!=total) return false;
+        }
+        return true;
+    }
+
+    int toInt(char[] chars, int s, int e){
+        int num=0;
+        for (int i=s; i<e; i++)
+            num=(num<<1)+chars[i]-'0';
+        return num;
     }
 }
--- src/leetcode/realtest/realTest20190324/SmallestIntegerDivisiblebyK.java
+++ src/leetcode/realtest/realTest20190324/SmallestIntegerDivisiblebyK.java
@@ -1,6 +1,5 @@
 package leetcode.realtest.realTest20190324;
 
-import java.math.BigInteger;
 
 /**
  * @author shibing
@@ -10,34 +9,27 @@ public class SmallestIntegerDivisiblebyK {
     public static void main(String[] args) {
         SmallestIntegerDivisiblebyK divisiblebyK=new SmallestIntegerDivisiblebyK();
         int K=9;
-//        K=1;
-//        K=2;
+        K=1;
+        K=2;
         K=3;
 //        System.out.println(Integer.MAX_VALUE);
         System.out.println(divisiblebyK.smallestRepunitDivByK(K));
     }
     public int smallestRepunitDivByK(int K) {
-        int[] set=new int[]{1,11,111,1111,11111,111111,1111111,11111111,111111111,1111111111};
-        int res=-1;
-        for (int x:set)
-            if(x>=K && x%K==0) {
-                res=countDigits(x);
-                break;
+        int sum=0, cnt=0;
+        do {
+            boolean flag=true;
+            for (int d =0; d<10; d++) {
+                int temp = sum + K * d ;
+                if (temp%10 == 1) {
+                    sum=temp/10;
+                    cnt++;
+                    flag=false;
+                    break;
+                }
             }
-//        BigInteger x=BigInteger.ONE;
-//        while (){
-//            if(x>=K && x%K==0) break;
-//            x=x*10+1;
-//
-//        }
-        return res;
-    }
-    public int countDigits(int N){
-        int cnt=1;
-        while (N/10!=0){
-            N/=10;
-            cnt++;
-        }
+            if(flag) return -1;
+        }while (sum>0);
         return cnt;
     }
 }
