093b54691891f9f58d54660157019ccc88855495
Maxatom <710909333@qq.com>
2019-04-09 23:23:49 +0800

    LongestIncreasingSubsequence

--- src/leetcode/problems/dynamicPrograming/LongestIncreasingSubsequence.java
+++ src/leetcode/problems/dynamicPrograming/LongestIncreasingSubsequence.java
@@ -4,6 +4,8 @@ import utils.RandomGenerator;
 import utils.PrintUtils;
 import utils.Utils;
 
+import java.util.Arrays;
+
 /**
  * @author shibing
  * @since 2018/12/22 12:22
@@ -19,9 +21,13 @@ public class LongestIncreasingSubsequence {
         int[] nums=new int[]{};
 //        nums=new int[]{5};
         nums=new int[]{10,9,2,5,3,7,101,18};
-        nums=new int[]{2,2};
+//        nums=new int[]{2,2};
         System.out.println(s+", result=" +subsequence.lengthOfLIS(nums));
-
+        System.out.println("result=" +subsequence.lengthOfLIS1(nums));
+//        System.out.println(Arrays.binarySearch(new int[]{1,3,5}, 0,3,0));
+//        System.out.println(Arrays.binarySearch(new int[]{1,3,5}, 0,3,2));
+//        System.out.println(Arrays.binarySearch(new int[]{1,3,5}, 0,3,4));
+//        System.out.println(Arrays.binarySearch(new int[]{1,3,5}, 0,3,6));
 
     }
     //DP   dp(i)=max(1, Max(dp[j]))  j=(1,i);
@@ -39,6 +45,24 @@ public class LongestIncreasingSubsequence {
         PrintUtils.printIntArray(dp);
         return Utils.max(dp);
     }
+
+
+    //O(nlogn) dp
+    public int lengthOfLIS1(int[] nums) {
+        if(nums.length==0) return 0;
+        int[] dp=new int[nums.length+1];
+        int max=1;
+        dp[1]=nums[0];
+        for (int i = 1; i <nums.length ; i++) {
+            int pos=Arrays.binarySearch(dp,1,max+1, nums[i]);
+            if(pos<0) pos=-pos-1;
+            dp[pos]=nums[i];
+            if(pos==max+1) ++max;
+        }
+        PrintUtils.printIntArray(dp);
+        return max;
+    }
+
     //for Integer O(N^2) dp
     public int lengthOfLIS(int[] nums) {
         if(nums.length==0) return 0;
