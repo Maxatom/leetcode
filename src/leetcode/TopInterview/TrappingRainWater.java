8b3ee7ae8f3386cb1220f9a6b6b43f736992ea4f
Maxatom <710909333@qq.com>
2019-06-14 14:26:48 +0800

    coding...

---
+++ src/leetcode/TopInterview/TrappingRainWater.java
@@ -0,0 +1,14 @@
+package leetcode.TopInterview;
+
+/**
+ * @author Shibing
+ * @since 2019-06-14 11:22:01
+ **/
+public class TrappingRainWater {
+    public static void main(String[] args) {
+
+    }
+    public int trap(int[] height) {
+
+    }
+}
