ec181b005169d44ef532df28adddf7b58f5c8a63
Maxatom <710909333@qq.com>
2019-06-27 13:54:10 +0800

    ShortestCommonSupersequence

--- src/leetcode/realtest/realTest20190616/ShortestCommonSupersequence.java
+++ src/leetcode/realtest/realTest20190616/ShortestCommonSupersequence.java
@@ -14,25 +14,35 @@ public class ShortestCommonSupersequence {
         String str1 = "abac", str2 = "cab";
 //        str1="bbbaaaba"; str2= "bbababbb";
 //        str1="aabbabaa"; str2= "aabbbbbbaa";
-        System.out.println(sequence.shortestCommonSupersequence(str1, str2));
+//        System.out.println(sequence.shortestCommonSupersequence(str1, str2));
         System.out.println(sequence.shortestCommonSupersequence1(str1, str2));
     }
 
     public String shortestCommonSupersequence1(String str1, String str2) {
         int n1=str1.length(), n2=str2.length();
         int[][] dp=new int[n1+1][n2+1];
-        for (int i = 0; i < n1; i++) {
-            for (int j = 0; j < n2; j++) {
-                if(i==0) dp[i+1][j+1]=j;
-                else if(j==0) dp[i+1][j+1]=i;
+        for (int i = -1; i < n1; i++) {
+            for (int j = -1; j < n2; j++) {
+                if(i==-1) dp[i+1][j+1]=j;
+                else if(j==-1) dp[i+1][j+1]=i;
                 else if(str1.charAt(i)==str2.charAt(j)) dp[i+1][j+1]=dp[i][j]+1;
                 else dp[i+1][j+1]=1+Math.min(dp[i+1][j],dp[i][j+1]);
             }
         }
-        PrintUtils.print2DIntArray(dp);
+//        PrintUtils.print2DIntArray(dp);
         StringBuilder sb=new StringBuilder();
-        int i=0, j=0;
-        return null;
+        int i=n1-1, j=n2-1;
+        while (i>=0&&j>=0){
+            if(str1.charAt(i)==str2.charAt(j)){
+                sb.append(str1.charAt(i)); i--; j--;
+            } else if(dp[i][j+1]<=dp[i+1][j]){
+                sb.append(str1.charAt(i)); i--;
+            } else{
+                sb.append(str2.charAt(j)); j--;
+            }
+        }
+//        System.out.println("sb="+sb.toString()+", i="+i+",j="+j);
+        return str1.substring(0,i+1)+str2.substring(0,j+1)+sb.reverse().toString();
     }
 
     public String shortestCommonSupersequence(String str1, String str2) {
@@ -45,7 +55,6 @@ public class ShortestCommonSupersequence {
                 else dp[i+1][j+1] = dp[i][j+1].length() > dp[i+1][j].length() ? dp[i][j+1] : dp[i+1][j];
             }
         }
-//        System.out.println(dp[n1][n2]);
         StringBuilder sb=new StringBuilder();
         int i=0, j=0;
         for (char c: dp[n1][n2].toCharArray()) {
